c7c1fb105e0b14d54b035b9c514c8347b92980f2
Elena Fioshkina <l14k15n12@gmail.com>
2023-07-17 10:03:57 +0300

    user profile working

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,31 @@
-import styles from "./Profile.module.css"
+import styles from "./Profile.module.css";
 
 export const Profile = (props) => {
-    let user = props.function(); 
-    console.log(user);
+	let userId = location.pathname.split("/")[2];
+  let user = props.function(userId);
 
-    return (
-        <div className="row">
-            <div className="col-md-4">
-                <img src={user.avatar} alt="" width="100%" />
-            </div>
-            <div className="col-md-8">
-                <h2 className={styles.id}>Id: <span>{user.id}</span> </h2>
-                <h3 className={styles.name}>Фамилия и имя: <span>{user.lastname} {user.name}</span> </h3>
-                <h3 className={styles.email}>Email: <span>{user.email}</span> </h3>
-                <p className={styles.about}>{user.about}
-                </p>
-                <button style={{backgroundColor: "green", pdding: "5px", borderRadius: "4px"}}>Отправить</button>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="row">
+			<div className="col-md-4">
+				<img
+					src={user.avatar}
+					alt=""
+          width="100%"
+				/>
+			</div>
+			<div className="col-md-8">
+				<h2 className={styles.id}>
+					Id: <span>{user.id}</span>
+				</h2>
+				<h3 className={styles.name}>
+					Фамилия, имя: <span>{user.lastname} {user.name}</span>
+				</h3>
+				<h3 className={styles.email}>
+					Email: <span>{user.email}</span>
+				</h3>
+				<p className={styles.about}>{user.about}</p>
+        <button style={{backgroundColor: "green", padding: "5px", borderRadius: "4px", borderColor: "green"}}>Отправить</button>
+			</div>
+		</div>
+	);
+};
