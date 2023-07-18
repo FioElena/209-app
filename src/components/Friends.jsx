b888185a5c60e94c0bf7fc042985ec3254ecaa95
Elena Fioshkina <l14k15n12@gmail.com>
2023-07-18 11:30:12 +0300

    class Friends, del friends

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-
+import React from "react";
 const TableRow = (props) => {
 	return (
 		<tr>
@@ -14,42 +14,56 @@ const TableRow = (props) => {
 	);
 };
 
-export const Friends = (props) => {
-	let users = props.function();
-
-	let usersCount = Object.keys(users).length;
-	let userRow = [];
-
-	for (let i = 0; i < usersCount; i++) {
-		userRow.push(
-			<TableRow
-				id={users[i].id}
-				index={i}
-				key={i}
-				name={users[i].name}
-				lastname={users[i].lastname}
-				email={users[i].email}
-			/>
-		);
+export class Friends extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { userRow: [] };
 	}
 
-	return (
-		<>
-			<h2 className="text-center">Список друзей:</h2>
-			<div className="row">
-				<div className="col-12 mx-auto">
-					<table className="table table-bordered">
-						<thead>
-							<tr>
-								<th scope="col">#</th>
-								<th scope="col">Имя и фамилия</th>
-								<th scope="col">Email</th>
-							</tr>
-						</thead>
-						<tbody>{userRow}</tbody>
-					</table>
+	componentDidMount() {
+		this.props.function().then((users) => {
+			console.log(users);
+
+			let usersCount = users.length;
+			let userRow = [];
+
+			for (let i = 0; i < usersCount; i++) {
+				userRow.push(
+					<TableRow
+						id={users[i].id}
+						index={i}
+						key={i}
+						name={users[i].name}
+						lastname={users[i].lastname}
+						email={users[i].email}
+					/>
+				);
+			}
+			this.setState({ userRow: userRow });
+		});
+
+	}
+
+	render() {
+		return (
+			<>
+				<h2 className="text-center">Список друзей:</h2>
+				<div className="row">
+					<div className="col-12 mx-auto">
+						<table className="table table-bordered">
+							<thead>
+								<tr>
+									<th scope="col">#</th>
+									<th scope="col">Имя и фамилия</th>
+									<th scope="col">Email</th>
+								</tr>
+							</thead>
+							<tbody>{this.state.userRow}</tbody>
+						</table>
+					</div>
 				</div>
-			</div>
-		</>
-	);
-};
+			</>
+		);
+
+	}
+}
