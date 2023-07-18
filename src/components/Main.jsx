ffc83a5ae97408de7fce6282223cf9590d8b1cec
Elena Fioshkina <l14k15n12@gmail.com>
2023-07-18 09:37:39 +0300

    add fetch to host

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,12 @@
-
+import image from ".././assets/img/8125378224jpg.jpg"
 export const Main = () => {
-    return <h1>Добро пожаловать!</h1>;
+    return (
+        <>
+        <h1 className="text-center">Добро пожаловать!</h1>
+        <div className="row mt-5">
+            <div className="col-md-6"><img src={image} alt="" width="100%" /></div>
+            <div className="col-md-6">Личный кабинет пользователя</div>
+        </div>
+        </>
+    );
 }
